ceb3e4c72bcbd9a2cb343e25d155107863fd58e0
youngff43 <99932948+youngff43@users.noreply.github.com>
2022-05-18 23:45:23 -0600

    End of lesson 2

--- server.js
+++ server.js
@@ -6,11 +6,8 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
-
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -83,7 +80,7 @@ app.get('/api/animals/:id', (req, res) => {
   if (result) {
     res.json(result);
   } else {
-    res.sendStatus(404);
+    res.send(404);
   }
 });
 
@@ -91,7 +88,6 @@ app.post('/api/animals', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  // if any data in req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
@@ -99,8 +95,6 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
-
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
